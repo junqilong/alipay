26e437bfa5d50ffe0793eb97d3236f7c8144d2d5
贺玲珑 <linglong.he@tcl.com>
2020-12-14 14:37:42 +0800

    first commit

---
+++ src/main/java/com/hll/alipay/service/AlipayTradeService.java
@@ -0,0 +1,28 @@
+package com.hll.alipay.service;
+
+import com.hll.alipay.model.builder.AlipayTradePayRequestBuilder;
+import com.hll.alipay.model.builder.AlipayTradePrecreateRequestBuilder;
+import com.hll.alipay.model.builder.AlipayTradeQueryRequestBuilder;
+import com.hll.alipay.model.builder.AlipayTradeRefundRequestBuilder;
+import com.hll.alipay.model.result.AlipayF2FPayResult;
+import com.hll.alipay.model.result.AlipayF2FPrecreateResult;
+import com.hll.alipay.model.result.AlipayF2FQueryResult;
+import com.hll.alipay.model.result.AlipayF2FRefundResult;
+
+/**
+ * Created by liuyangkly on 15/7/29.
+ */
+public interface AlipayTradeService {
+
+    // 当面付2.0流程支付
+    public AlipayF2FPayResult tradePay(AlipayTradePayRequestBuilder builder);
+
+    // 当面付2.0消费查询
+    public AlipayF2FQueryResult queryTradeResult(AlipayTradeQueryRequestBuilder builder);
+
+    // 当面付2.0消费退款
+    public AlipayF2FRefundResult tradeRefund(AlipayTradeRefundRequestBuilder builder);
+
+    // 当面付2.0预下单(生成二维码)
+    public AlipayF2FPrecreateResult tradePrecreate(AlipayTradePrecreateRequestBuilder builder);
+}
